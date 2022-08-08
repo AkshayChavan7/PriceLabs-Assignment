e383ea60a84460f748199b5c6cc0811bdca8eef2
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2022-08-08 18:33:00 +0530

    layout design for listing

--- Client/src/components/side-panel/tabs-container/TabsContainer.js
+++ Client/src/components/side-panel/tabs-container/TabsContainer.js
@@ -1,62 +1,86 @@
-import React from 'react';
-import './TabsContainer.css';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import React from "react";
+import "./TabsContainer.css";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { Checkbox } from "@mui/material";
+import Listings from "../listings/Listings";
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
+  const { children, value, index, ...other } = props;
 
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 0.1 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const TabsContainer = (props) => {
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-    
-    return <Box sx={{ width: '100%' }}>
-    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <Tabs  value={value} onChange={handleChange} varia-label="basic tabs example">
-        <Tab style={{textTransform: "none"}} label="Comp Set" {...a11yProps(0)}/>
-        <Tab style={{textTransform: "none"}} label="Other properties" {...a11yProps(1)}/>
-        <Tab style={{textTransform: "none"}} label="Hotels" {...a11yProps(2)}/>
-      </Tabs>
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          varia-label="basic tabs example"
+        >
+          <Tab
+            style={{ textTransform: "none", fontSize: 12 }}
+            label="Comp Set (49)"
+            {...a11yProps(0)}
+          />
+          <Tab
+            style={{ textTransform: "none", fontSize: 12 }}
+            label="Other properties (951)"
+            {...a11yProps(1)}
+          />
+          <Tab
+            style={{ textTransform: "none", fontSize: 12 }}
+            label="Hotels (52)"
+            {...a11yProps(2)}
+          />
+        </Tabs>
+      </Box>
+      <div style={{ color: "#787878", marginTop: 10 }}>
+        {" "}
+        <Checkbox size="small" /> Select all properties
+      </div>
+      <TabPanel value={value} index={0}>
+        <Listings />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
     </Box>
-    <TabPanel value={value}  index={0}>
-      Item One
-    </TabPanel>
-    <TabPanel value={value} index={1}>
-      Item Two
-    </TabPanel>
-    <TabPanel value={value}  index={2}>
-      Item Three
-    </TabPanel>
-  </Box>
+  );
 };
 
 export default TabsContainer;
